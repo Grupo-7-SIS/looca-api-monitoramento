ca6103f537bad2df1764265f0f987d70bb2bcdf6
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2025-09-29 15:35:38 -0300

    add: estrutura de projeto (DTO, Entity, Controller e Main)

--- looca-api-prod/src/main/java/school/sptech/Main.java
+++ looca-api-prod/src/main/java/school/sptech/Main.java
@@ -2,17 +2,14 @@ package school.sptech;
 
 import com.github.britooo.looca.api.core.Looca;
 import com.github.britooo.looca.api.group.rede.RedeInterface;
+import school.sptech.controller.LoocaCapturaController;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Looca looca = new Looca();
-
-        List<RedeInterface> dados = looca.getRede().getGrupoDeInterfaces().getInterfaces();
-
-        for (RedeInterface dado : dados) {
-            System.out.println(dado);
-        }
+        // Looca looca = new Looca();
+        LoocaCapturaController loocaDados = new LoocaCapturaController();
+        loocaDados.dadosNetworking();
     }
 }
