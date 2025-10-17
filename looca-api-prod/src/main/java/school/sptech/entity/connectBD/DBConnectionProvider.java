30739ad109b5317a3686d75a4493fd56a384c7b4
henriquedouradoo <henrique.domingos@sptech.school>
2025-10-17 12:33:51 -0300

    add: dotenv com variaveis de ambiente

--- looca-api-prod/src/main/java/school/sptech/entity/connectBD/DBConnectionProvider.java
+++ looca-api-prod/src/main/java/school/sptech/entity/connectBD/DBConnectionProvider.java
@@ -2,6 +2,7 @@ package school.sptech.entity.connectBD;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class DBConnectionProvider {
 
@@ -10,9 +11,13 @@ public class DBConnectionProvider {
 
     public DBConnectionProvider() {
         BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl("jdbc:mysql://localhost:3306/cyberbeef?useSSL=false&serverTimezone=UTC");
-        basicDataSource.setUsername("root");  // seu usuário do MySQL
-        basicDataSource.setPassword("stevejobs");  // sua senha do MySQL
+        Dotenv dotenv = Dotenv.load();
+        String ip = dotenv.get("IP_BANCO");
+        String user = dotenv.get("USUARIO");
+        String password = dotenv.get("PASSWORD");
+        basicDataSource.setUrl("jdbc:mysql://" + ip + ":3306/cyberbeef?useSSL=false&serverTimezone=UTC");
+        basicDataSource.setUsername(user);  // seu usuário do MySQL
+        basicDataSource.setPassword(password);  // sua senha do MySQL
         basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 
         this.basicDataSource = basicDataSource;
